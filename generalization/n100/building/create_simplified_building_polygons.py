d6e4285edfef9827e24889b001a946323043e16a
maribhe <mari.helland@kartverket.no>
2023-10-17 15:46:09 +0200

    Moved code into one function

--- generalization/n100/building/create_simplified_building_polygons.py
+++ generalization/n100/building/create_simplified_building_polygons.py
@@ -12,24 +12,21 @@ import arcpy
 # Importing environment
 environment_setup.setup(workspace=config.n100_building_workspace)
 
-
-#           *************************    CURRENT WORKFLOW   ****************************
-
 # Main function
 def main(): 
-    aggregate_polygon()
-    simplify_building()
-    simplify_polygon()
-    simplify_building()
+    simplify_building_polygon()
+    
 
 
-# Aggregating building polygons 
+def simplify_building_polygon():
 
-def aggregate_polygon():
+    # Aggregating building polygons
+    
+    output_aggregate_polygon = "aggregated_polygon"
 
     arcpy.cartography.AggregatePolygons(
         in_features=input_n50.Grunnriss,
-        out_feature_class="aggregated_polygons", 
+        out_feature_class=output_aggregate_polygon, 
         aggregation_distance="15 Meters", 
         minimum_area="3200 SquareMeters", 
         minimum_hole_size="10000 SquareMeters", 
@@ -38,83 +35,55 @@ def aggregate_polygon():
         out_table="grunnriss_n50_aggregated_tbl", 
         aggregate_field="BYGGTYP_NBR") 
     
+
+    # Simplifying building polygons
     
-# Simpliifying building polygons and generating points for polygons that are smaller than the specified minimum size
-
-def simplify_building():
-
+    output_simplify_building = "simplified_building"
+    
     SimplifyBuilding_model_step1_Pnt = arcpy.cartography.SimplifyBuilding(
-    in_features="aggregated_polygons",
-    out_feature_class="simplified_buildings_1"
-    simplification_tolerance="75", 
-    minimum_area="3200 SquareMeters", 
-    collapsed_point_option="KEEP_COLLAPSED_POINTS")
+        in_features=output_aggregate_polygon,
+        out_feature_class=output_simplify_building,
+        simplification_tolerance="75", 
+        minimum_area="3200 SquareMeters", 
+        collapsed_point_option="KEEP_COLLAPSED_POINTS")
+    
+    
+    # Simplifying polygons
 
-# Simpliifying polygons and generating points for polygons that are smaller than the specified minimum size
+    output_simplify_polygon = "simplified_polygon"
 
-def simplify_polygon(buildings):
     Grunnriss_simplify_polygon_m1_Pnt = arcpy.cartography.SimplifyPolygon(
-    in_features=buildings,
-    out_feature_class="simplified_polygons",
-    algorithm="WEIGHTED_AREA",
-    simplification_tolerance="15",
-    minimum_area="3200 SquareMeters",
-    collapsed_point_option="KEEP_COLLAPSED_POINTS")
+        in_features=output_simplify_building,
+        out_feature_class=output_simplify_polygon,
+        algorithm="WEIGHTED_AREA",
+        simplification_tolerance="15",
+        minimum_area="3200 SquareMeters",
+        collapsed_point_option="KEEP_COLLAPSED_POINTS")
+    
+    # Spatial join between simplified building polygons and original building polygons
 
-# Spatial join between simplified building polygons and original building polygons 
-       
-def spatial_join(): 
-    arcpy.analysis.SpatialJoin(target_features="simplified_buildings_1", 
+    output_spatial_join = "spatial_joined_polygon"
+
+    arcpy.analysis.SpatialJoin(target_features=output_simplify_polygon, 
                                join_features=input_n50.Grunnriss, 
-                               out_feature_class="spatial_joined_polygon_1")
-
-
-# Adding multiple fields 
-
-def add_fields(buildings): 
+                               out_feature_class=output_spatial_join)
+    
+    
+    # Adding multiple fields
     arcpy.management.AddFields(
-        in_table=buildings, 
+        in_table=output_spatial_join, 
         field_description=[["angle", "SHORT"], 
                            ["hierarchy", "SHORT"], 
                            ["invisibility", "SHORT"]])
     
-# Assigning values to the fields 
-
-def calculate_fields(buildings): 
+    # Assigning values to the fields
     arcpy.management.CalculateFields(
-        in_features=buildings,
+        in_features=output_spatial_join,
         expression_type="PYTHON3", 
         fields=[["angle", "0"], 
                 ["hierarchy", "0"], 
                 ["invisibility", "0"]])
 
-# Making a copy of the feature class 
+    # Making a copy of the feature class
+    arcpy.management.CopyFeatures(output_spatial_join, "simplified_grunnriss_n50")
 
-def copy_features():
-    arcpy.management.CopyFeatures(in_features, out_feature_class)
-
-
-
-
-#            *************************    PARAMETERIZED FUNCTIONS   ****************************
-
-
-
-def simplify_building_parameterized(buildings, out_feature_class, simplification_tolerance, minimum_area, collapsed_point_option):
-    SimplifyBuilding_model_step1_Pnt = arcpy.cartography.SimplifyBuilding(
-        in_features=buildings,
-        out_feature_class=out_feature_class,
-        simplification_tolerance=simplification_tolerance,
-        minimum_area=minimum_area,
-        collapsed_point_option=collapsed_point_option
-    )
-
-def simplify_polygon_parameterized(buildings, out_feature_class, algorithm, simplification_tolerance, minimum_area, collapsed_point_option):
-    Grunnriss_simplify_polygon_m1_Pnt = arcpy.cartography.SimplifyPolygon(
-        in_features=buildings,
-        out_feature_class=out_feature_class,
-        algorithm=algorithm,
-        simplification_tolerance=simplification_tolerance,
-        minimum_area=minimum_area,
-        collapsed_point_option=collapsed_point_option
-    )
