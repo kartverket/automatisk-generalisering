83adb2bbfdf120ca7e29fb784f7aa3fbfe91ba17
maribhe <mari.helland@kartverket.no>
2023-12-15 10:50:46 +0100

    clean_up_building

--- generalization/n100/building/building_main.py
+++ generalization/n100/building/building_main.py
@@ -10,6 +10,7 @@ from generalization.n100.building import create_simplified_building_polygons
 from generalization.n100.building import create_points_from_polygon
 from generalization.n100.building import points_to_polygon
 from generalization.n100.building import resolve_building_conflicts
+from generalization.n100.building import clean_up_building
 
 # Importing environment
 environment_setup.general_setup()
@@ -23,6 +24,7 @@ def main():
     calculating_values.main()
     points_to_polygon.main()
     resolve_building_conflicts.main()
+    # clean_up_building.main()
 
 
 if __name__ == "__main__":
