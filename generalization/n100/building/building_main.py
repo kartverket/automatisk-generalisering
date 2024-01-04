2f3e2917f78c6f9e9e00555f55cfe866f64e3fcd
maribhe <mari.helland@kartverket.no>
2024-01-04 13:31:48 +0100

    cleanup

--- generalization/n100/building/building_main.py
+++ generalization/n100/building/building_main.py
@@ -1,30 +1,33 @@
-# Importing environment and directory structure modules
+# Importing environment and directory structure
 from env_setup import environment_setup
 from env_setup import setup_directory_structure
 
-
-# Importing sub models
-from generalization.n100.building import building_data_preparation
-from generalization.n100.building import calculating_values
-from generalization.n100.building import create_simplified_building_polygons
-from generalization.n100.building import create_points_from_polygon
-from generalization.n100.building import points_to_polygon
-from generalization.n100.building import resolve_building_conflicts
-from generalization.n100.building import clean_up_building
-
-# Importing environment
+# Environment and directory structure setup
 environment_setup.general_setup()
 setup_directory_structure.main()
 
 
+# Importing building scripts
+from generalization.n100.building import building_data_preparation
+from generalization.n100.building import calculating_values
+from generalization.n100.building import create_simplified_building_polygons
+from generalization.n100.building import create_points_from_polygon
+from generalization.n100.building import hospital_church_clusters
+from generalization.n100.building import points_to_polygon
+from generalization.n100.building import resolve_building_conflicts
+from generalization.n100.building import clean_up_building
+
+
+# Main function that runs all the building scripts
 def main():
     building_data_preparation.main()
     create_simplified_building_polygons.main()
     create_points_from_polygon.main()
     calculating_values.main()
+    hospital_church_clusters.main()
     points_to_polygon.main()
     resolve_building_conflicts.main()
-    # clean_up_building.main()
+    clean_up_building.main()
 
 
 if __name__ == "__main__":
