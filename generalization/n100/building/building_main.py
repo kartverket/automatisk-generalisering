bbd865bb47a7db70ba328b28a96607d2dc0bc039
maribhe <mari.helland@kartverket.no>
2024-02-23 09:40:41 +0100

    docstring

--- generalization/n100/building/building_main.py
+++ generalization/n100/building/building_main.py
@@ -27,12 +27,25 @@ def main():
     This is the main function that runs all the building scripts.
 
     Summary:
-    1. building_data_preparation: Aims to prepare the data for future building generalization processing.
-    2. create_simplified_building_polygons: Aggregates and simplifies building polygons, minimizing detailed parts of the building.
-    3. building_polygon_displacement:
+    1. building_data_preparation:
+        Aims to prepare the data for future building generalization processing.
+
+    2. create_simplified_building_polygons:
+        Aggregates and simplifies building polygons, minimizing detailed parts of the building.
+
+    3. building_polygon_displacement_rbc:
+        Aligns building polygons with roads at a 1:100,000 scale, resolves conflicts among all building polygons,
+         considering various barriers, and outputs both polygon and point layers.
+
     4. create_points_from_polygon
+        Creates points from small grunnriss lost during aggregation, and merges
+        them together with collapsed points from the tools simplify building and simplify polygon.
+
     5. calculating_values
-    6. propagate_displacement
+        Adds required fields for building point for symbology and resolves building conflicts: angle, hierarchy, and invisibility.
+
+    6. propagate_displacement:
+
     7. building_point_buffer_displacement
     8. hospital_church_clusters
     9. points_to_polygon
