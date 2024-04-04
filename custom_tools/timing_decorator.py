1459d428ad96c6991e48bc9ed7039578cbaf6926
maribhe <mari.helland@kartverket.no>
2024-04-04 13:40:37 +0200

    timing_decorator update

--- custom_tools/timing_decorator.py
+++ custom_tools/timing_decorator.py
@@ -1,17 +1,12 @@
 import time
 from functools import wraps
 
-# Importing temporary files
-from file_manager.n100.file_manager_buildings import Building_N100
-
-import time
-from functools import wraps
-
-# Importing temporary files
+# Importing file manager
 from file_manager.n100.file_manager_buildings import Building_N100
 
 # List to store print statements
 print_output = []
+
 
 # Decorator to measure execution time of functions
 def timing_decorator(arg=None):
@@ -52,24 +47,13 @@ def timing_decorator(arg=None):
         return wrapper
 
 
-# Total elapsed time accumulator
-total_elapsed_time = 0
+def write_to_file():
+    output_file = Building_N100.overview__runtime_all_building_functions__n100.value
+    with open(output_file, "w") as f:
+        for output in print_output:
+            f.write(output + "\n")
 
-# Calculate total elapsed time
-for line in print_output:
-    minutes = int(line.split(":")[1].split()[0])
-    seconds = float(line.split(":")[1].split()[2])
-    total_elapsed_time += minutes * 60 + seconds
 
-# Write all print statements to a file
-output_file = Building_N100.overview__runtime_all_building_functions__n100.value
-
-# Write total elapsed time to the file
-with open(output_file, "w") as f:
-    f.write(
-        f"Total run time: {int(total_elapsed_time // 3600)} hours {int((total_elapsed_time % 3600) // 60)} minutes {total_elapsed_time % 60:.2f} seconds\n\n"
-    )
-
-    # Write all print statements to the file with additional newline characters
-    for line in print_output:
-        f.write(line + "\n")
+def clear_print_output():
+    global print_output
+    print_output = []
