8ee4dca6df4101dec29ce6bcd3113b22c3cae01e
EllingOftedalKV <elling.aronsen.oftedal@kartverket.no>
2024-04-12 12:20:37 +0200

    Update timing_decorator to improve logging details
    The timing_decorator has been amended to provide more detailed logging. This includes logging the file name and function name along with their execution time. Additionally, the log file is now cleared at the start of each runtime which prevents old log data from persisting.

--- custom_tools/timing_decorator.py
+++ custom_tools/timing_decorator.py
@@ -1,8 +1,12 @@
 import time
+import inspect
 import os
 from functools import wraps
 
 from file_manager.n100.file_manager_buildings import Building_N100
+
+
+TIMING_DECORATOR_LOG_FILE_USED = False
 
 
 def timing_decorator(func):
@@ -16,7 +20,17 @@ def timing_decorator(func):
 
         elapsed_time = compute_elapsed_time(start_time)
 
-        log_to_console_and_file(func.__name__, elapsed_time)
+        function_name = func.__name__
+        file_path = inspect.getfile(func)
+        file_name = os.path.basename(file_path)
+
+        formatted_file_name = file_name.ljust(40)
+        formatted_function_name = function_name.ljust(55)
+
+        log_to_console_and_file(
+            f"File name: {formatted_file_name} Function name: {formatted_function_name}",
+            elapsed_time,
+        )
 
         return result
 
@@ -27,7 +41,7 @@ def compute_elapsed_time(start_time):
     """Computes the elapsed time given a starting time"""
     elapsed_time_seconds = time.time() - start_time
 
-    elapsed_minutes, elapsed_seconds = divmod(elapsed_time_seconds, 60)
+    elapsed_minutes, elapsed_seconds = divmod(elapsed_time_seconds, 30)
 
     return elapsed_minutes, elapsed_seconds
 
@@ -35,7 +49,9 @@ def compute_elapsed_time(start_time):
 def log_to_console_and_file(function_name, elapsed_time):
     """Logs a messages to both the console and a file"""
     elapsed_minutes, elapsed_seconds = elapsed_time
-    output = f"{function_name} execution time: {int(elapsed_minutes)} minutes {elapsed_seconds:.0f} seconds"
+    output = f"{function_name} Execution time: {int(elapsed_minutes)} minutes {elapsed_seconds:.0f} seconds".ljust(
+        60
+    )
 
     log_to_console(output)
     log_to_file(output)
@@ -48,6 +64,16 @@ def log_to_console(message):
 
 def log_to_file(message):
     """Writes a given message to a log file"""
+
+    global TIMING_DECORATOR_LOG_FILE_USED
+
     log_file_path = Building_N100.overview__runtime_all_building_functions__n100.value
+
+    if os.path.exists(log_file_path) and not TIMING_DECORATOR_LOG_FILE_USED:
+        # if it's the first time this runtime, delete the log file
+        os.remove(log_file_path)
+
+    TIMING_DECORATOR_LOG_FILE_USED = True
+
     with open(log_file_path, "a") as f:
         f.write(message + "\n")
