36986988d94c76774436dd4b0c0c9a8ae95792a0
maribhe <mari.helland@kartverket.no>
2024-05-02 12:43:09 +0200

    removed road buffers, we use line instead

--- input_data/input_symbology.py
+++ input_data/input_symbology.py
@@ -6,7 +6,6 @@ import config
 # Defining universal paths for other files regardless of local path env_setup
 class SymbologyN100(Enum):
     veg_sti = config.symbology_n100_veg_sti
-    road_buffers = config.symbology_n100_road_buffer
     begrensnings_kurve_buffer = config.symbology_n100_begrensningskurve_buffer
     begrensnings_kurve_line = config.symbology_n100_begrensningskurve_line
     bygningspunkt = config.symbology_n100_bygningspunkt
