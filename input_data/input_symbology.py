b360756583edaa88822dd90a0e8378828f8a6a2b
maribhe <mari.helland@kartverket.no>
2024-05-02 12:42:32 +0200

    changed name

--- input_data/input_symbology.py
+++ input_data/input_symbology.py
@@ -6,10 +6,11 @@ import config
 # Defining universal paths for other files regardless of local path env_setup
 class SymbologyN100(Enum):
     veg_sti = config.symbology_n100_veg_sti
+    road_buffers = config.symbology_n100_road_buffer
     begrensnings_kurve_buffer = config.symbology_n100_begrensningskurve_buffer
     begrensnings_kurve_line = config.symbology_n100_begrensningskurve_line
     bygningspunkt = config.symbology_n100_bygningspunkt
     grunnriss = config.symbology_n100_grunnriss
     drawn_polygon = config.symbology_n100_drawn_polygon
-    railway_stations = config.symbology_railway_station
-    railways = config.symbology_railway_buffer
+    railway_station_squares = config.symbology_n100_railway_station_squares
+    railways = config.symbology_n100_railway_buffer
