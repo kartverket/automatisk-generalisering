cf7ade65a45eb7abbf68c5bb2ceb6668187a29ac
EllingOftedalKV <elling.aronsen.oftedal@kartverket.no>
2024-05-30 14:02:08 +0200

    Move 'compare_feature_classes' function to 'file_utilities.py'
    The 'compare_feature_classes' function was removed from its initial location in 'compare_feature_classes.py' and added to 'file_utilities.py' to improve organization. Import paths in the 'removing_overlapping_points.py' file were updated to reflect this change.

--- generalization/n100/building/removing_overlapping_points.py
+++ generalization/n100/building/removing_overlapping_points.py
@@ -5,7 +5,7 @@
 # Import custom modules
 from custom_tools.general_tools import custom_arcpy
 from env_setup import environment_setup
-from custom_tools.general_tools.compare_feature_classes import compare_feature_classes
+from custom_tools.general_tools.file_utilities import compare_feature_classes
 from custom_tools.general_tools.polygon_processor import PolygonProcessor
 from constants.n100_constants import N100_Symbology
 
