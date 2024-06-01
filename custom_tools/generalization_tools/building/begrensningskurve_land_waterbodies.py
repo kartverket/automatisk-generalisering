0887c288ce49f57e2d4e704e3acaee0759e2f923
EllingOftedalKV <elling.aronsen.oftedal@kartverket.no>
2024-06-01 09:39:20 +0200

    Remove unused imports in begrensningskurve_land_waterbodies.py
    Cleaned up begrensningskurve_land_waterbodies.py file by removing five unused imports. This change improves code readability and efficiency by cutting out the clutter. It also reduces the chance of importing unnecessary or conflicting modules.

--- custom_tools/generalization_tools/building/begrensningskurve_land_waterbodies.py
+++ custom_tools/generalization_tools/building/begrensningskurve_land_waterbodies.py
@@ -1,17 +1,12 @@
 import arcpy
 
-from input_data import input_n50
 from input_data import input_n100
-from input_data import input_other
 
 # Importing custom modules
 from file_manager.n100.file_manager_buildings import Building_N100
 from env_setup import environment_setup
-from custom_tools.decorators.timing_decorator import timing_decorator
 from custom_tools.decorators.partition_io_decorator import partition_io_decorator
 from custom_tools.general_tools import custom_arcpy
-from custom_tools.general_tools.polygon_processor import PolygonProcessor
-from input_data import input_symbology
 from constants.n100_constants import N100_Symbology, N100_SQLResources, N100_Values
 
 
