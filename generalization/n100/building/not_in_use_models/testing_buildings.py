e1581e933013e273078c65c68a49ec94b69cf961
EllingOftedalKV <elling.aronsen.oftedal@kartverket.no>
2024-06-05 17:05:04 +0200

    testing_buildings.py done ready to be implemented into buffer_displacement.py
    testing_buildings.py done ready to be implemented into buffer_displacement.py

--- generalization/n100/building/not_in_use_models/testing_buildings.py
+++ generalization/n100/building/not_in_use_models/testing_buildings.py
@@ -1,20 +1,35 @@
 class BufferIncrementCalculator:
     def __init__(
         self,
-        largest_road_dimension,
         buffer_displacement_meter,
-        maximum_buffer_increase_tolerance,
     ):
-        self.largest_road_dimension = largest_road_dimension
+        self.largest_road_dimension = None
         self.buffer_displacement_meter = buffer_displacement_meter
-        self.maximum_buffer_increase_tolerance = maximum_buffer_increase_tolerance
-        self.tolerance = maximum_buffer_increase_tolerance - 0.5
-        self.target_value = largest_road_dimension + buffer_displacement_meter
+        self.maximum_buffer_increase_tolerance = None
+        self.tolerance = None
+        self.target_value = None
         self.previous_value = 0
         self.current_value = 0
         self.rest_value = 0
         self.iteration_fixed_buffer_addition = 0
         self.increments = []
+
+    def finding_dimensions(self, buffer_displacement_meter):
+        """
+        Finds the smallest building symbol dimension and the largest road dimension.
+        """
+        smallest_building_dimension = 45
+        maximum_buffer_increase_tolerance = smallest_building_dimension / 2
+
+        self.maximum_buffer_increase_tolerance = maximum_buffer_increase_tolerance
+        self.tolerance = self.maximum_buffer_increase_tolerance - 1
+
+        largest_road_dimension = 43
+        self.largest_road_dimension = largest_road_dimension
+
+        self.maximum_buffer_increase_tolerance = maximum_buffer_increase_tolerance
+
+        self.target_value = self.largest_road_dimension + buffer_displacement_meter
 
     def calculate_buffer_increments(self):
         iteration_buffer_factor = 0
@@ -34,6 +49,7 @@ class BufferIncrementCalculator:
             if increment_value >= self.tolerance:
                 if not found_valid_increment:
                     iteration_buffer_factor = next_buffer_factor - 0.001
+                iteration_buffer_factor = round(iteration_buffer_factor, 3)
                 self.increments.append((iteration_buffer_factor, 0))
                 self.current_value = (
                     iteration_buffer_factor * self.largest_road_dimension
@@ -53,6 +69,7 @@ class BufferIncrementCalculator:
 
             self.rest_value = self.tolerance - increase_from_last_cleanup
 
+            self.rest_value = round(self.rest_value, 1)
             self.increments.append((1, self.rest_value))
             self.current_value = self.rest_value
         else:
@@ -76,13 +93,12 @@ class BufferIncrementCalculator:
 
         return self.increments
 
+    def run(self):
+        self.finding_dimensions(self.buffer_displacement_meter)
+        increments = self.calculate_buffer_increments()
+        print(increments)
 
-# Example usage
-calculator = BufferIncrementCalculator(
-    largest_road_dimension=43,
-    buffer_displacement_meter=30,
-    maximum_buffer_increase_tolerance=22.5,
-)
-increments = calculator.calculate_buffer_increments()
 
-print(increments)
+if __name__ == "__main__":
+    buffer_calculator = BufferIncrementCalculator(buffer_displacement_meter=30)
+    buffer_calculator.run()
