c72c9df017f8e71bc7047908c3ec6d137c41bf94
EllingOftedalKV <elling.aronsen.oftedal@kartverket.no>
2024-06-05 17:06:45 +0200

    implemented logic to find params to create buffer increase dynamically in buffer_displacement.py
    implemented logic to find params to create buffer increase dynamically in buffer_displacement.py

--- custom_tools/generalization_tools/building/buffer_displacement.py
+++ custom_tools/generalization_tools/building/buffer_displacement.py
@@ -15,20 +15,28 @@ class PointDisplacementUsingBuffers:
         input_road_lines: str,
         sql_selection_query: dict,
         output_road_buffer_base: str,
-        buffer_factors: List[Union[int, float]],
         buffer_displacement_meter: int = 30,
-        fixed_buffer_addition: Union[int, float] = 0,
         building_symbol_dimensions: Dict[int, Tuple[int, int]] = None,
     ):
         self.input_road_lines = input_road_lines
         self.sql_selection_query = sql_selection_query
         self.output_road_buffer_base = output_road_buffer_base
-        self.buffer_factors = buffer_factors
+
         self.buffer_displacement_meter = buffer_displacement_meter
-        self.fixed_buffer_addition = fixed_buffer_addition
         self.building_symbol_dimensions = building_symbol_dimensions
 
-    def finding_dimensions(self) -> Tuple[int, int]:
+        self.largest_road_dimension = None
+        self.buffer_displacement_meter = buffer_displacement_meter
+        self.maximum_buffer_increase_tolerance = None
+        self.tolerance = None
+        self.target_value = None
+        self.previous_value = 0
+        self.current_value = 0
+        self.rest_value = 0
+        self.iteration_fixed_buffer_addition = 0
+        self.increments = []
+
+    def finding_dimensions(self, buffer_displacement_meter):
         """
         Finds the smallest building symbol dimension and the largest road dimension.
         """
@@ -38,78 +46,105 @@ class PointDisplacementUsingBuffers:
         smallest_building_dimension = min(
             min(dimensions) for dimensions in self.building_symbol_dimensions.values()
         )
-        largest_road_dimension = max(self.sql_selection_query.values())
-
-        return smallest_building_dimension, largest_road_dimension
-
-    def calculate_iterations(
-        self, smallest_building_dimension: int, largest_road_dimension: int
-    ) -> List[Tuple[float, float]]:
-        """
-        Calculate the necessary buffer factors for the iterations.
-        """
         maximum_buffer_increase_tolerance = smallest_building_dimension / 2
-        total_buffer_needed = self.buffer_displacement_meter
-        largest_buffer = (largest_road_dimension * 1) + self.buffer_displacement_meter
 
-        # Determine the number of iterations needed
-        num_iterations = int(largest_buffer / maximum_buffer_increase_tolerance) + 1
-        increments = []
+        self.maximum_buffer_increase_tolerance = maximum_buffer_increase_tolerance
+        self.tolerance = self.maximum_buffer_increase_tolerance - 1
+        self.largest_road_dimension = max(self.sql_selection_query.values())
 
-        current_buffer = 0
-        for i in range(num_iterations - 1):
-            # Calculate the remaining buffer needed to avoid exceeding the total buffer
-            remaining_buffer_needed = largest_buffer - current_buffer
-            buffer_increase = min(
-                maximum_buffer_increase_tolerance - 1, remaining_buffer_needed
-            )
-            increments.append(buffer_increase)
-            current_buffer += buffer_increase
+        self.maximum_buffer_increase_tolerance = maximum_buffer_increase_tolerance
 
-        # Ensure the final increment meets the exact buffer displacement needed
-        final_increment = total_buffer_needed - sum(increments)
-        increments.append(final_increment)
+        self.target_value = self.largest_road_dimension + buffer_displacement_meter
 
-        # Create iterations with buffer factors and fixed buffer additions
-        iterations = [
-            (increment / largest_road_dimension, 0) for increment in increments[:-1]
-        ]
-        iterations.append((1, increments[-1]))
+    def calculate_buffer_increments(self):
+        iteration_buffer_factor = 0
 
-        # Print debug information
-        print(f"buffer displacement: {self.buffer_displacement_meter}")
-        print("Increments for each iteration:", increments)
-        print("Calculated iterations:", iterations)
+        found_valid_increment = False
 
-        return iterations
+        while iteration_buffer_factor < 1:
+            next_buffer_factor = iteration_buffer_factor + 0.001
 
-    def process_buffer_factor(self, factor: Union[int, float]):
+            if not found_valid_increment:
+                increment_value = next_buffer_factor * self.largest_road_dimension
+            else:
+                increment_value = (
+                    next_buffer_factor * self.largest_road_dimension
+                ) - self.previous_value
+
+            if increment_value >= self.tolerance:
+                if not found_valid_increment:
+                    iteration_buffer_factor = next_buffer_factor - 0.001
+                iteration_buffer_factor = round(iteration_buffer_factor, 3)
+                self.increments.append((iteration_buffer_factor, 0))
+                self.current_value = (
+                    iteration_buffer_factor * self.largest_road_dimension
+                )
+                self.previous_value = self.current_value
+                found_valid_increment = True
+                iteration_buffer_factor = next_buffer_factor
+
+                continue
+
+            iteration_buffer_factor = next_buffer_factor
+            self.current_value = iteration_buffer_factor * self.largest_road_dimension
+
+        if self.previous_value != self.current_value:
+            self.current_value = self.largest_road_dimension
+            increase_from_last_cleanup = self.current_value - self.previous_value
+
+            self.rest_value = self.tolerance - increase_from_last_cleanup
+
+            self.rest_value = round(self.rest_value, 1)
+            self.increments.append((1, self.rest_value))
+            self.current_value = self.rest_value
+        else:
+            self.current_value = 0
+
+        self.target_value = self.buffer_displacement_meter
+
+        while self.current_value <= self.target_value:
+            missing_value = self.target_value - self.current_value
+
+            if missing_value <= self.tolerance:
+                self.increments.append((1, self.buffer_displacement_meter))
+
+                break
+
+            increment_value = min(self.tolerance, missing_value)
+            self.iteration_fixed_buffer_addition = increment_value + self.current_value
+
+            self.increments.append((1, self.iteration_fixed_buffer_addition))
+            self.current_value = self.iteration_fixed_buffer_addition
+
+        print(self.increments)
+
+        return self.increments
+
+    def process_buffer_factor(
+        self, factor: Union[int, float], fixed_addition: Union[int, float]
+    ):
         """
         Processes a single buffer factor by creating buffers and displacing points.
         """
         factor_name = str(factor).replace(".", "_")
-        output_road_buffer = f"{self.output_road_buffer_base}_factor_{factor_name}"
+        fixed_addition_name = str(fixed_addition).replace(".", "_")
+
+        output_road_buffer = f"{self.output_road_buffer_base}_factor_{factor_name}_add_{fixed_addition_name}"
         line_to_buffer_symbology = LineToBufferSymbology(
             input_road_lines=self.input_road_lines,
             sql_selection_query=self.sql_selection_query,
             output_road_buffer=output_road_buffer,
             buffer_factor=factor,
-            fixed_buffer_addition=self.fixed_buffer_addition,
+            fixed_buffer_addition=fixed_addition,
         )
         line_to_buffer_symbology.run()
 
     def run(self):
-        smallest_building_dimension, largest_road_dimension = self.finding_dimensions()
-        print(f"Smallest building symbol dimension: {smallest_building_dimension}")
-        print(f"Largest road dimension: {largest_road_dimension}")
+        self.finding_dimensions(self.buffer_displacement_meter)
+        self.calculate_buffer_increments()
 
-        iterations = self.calculate_iterations(
-            smallest_building_dimension, largest_road_dimension
-        )
-        print(f"Calculated iterations: {iterations}")
-
-        # for factor in self.buffer_factors:
-        #     self.process_buffer_factor(factor)
+        for factor, addition in self.increments:
+            self.process_buffer_factor(factor, addition)
 
 
 if __name__ == "__main__":
@@ -119,7 +154,6 @@ if __name__ == "__main__":
         input_road_lines=Building_N100.data_preparation___unsplit_roads___n100_building.value,
         sql_selection_query=N100_SQLResources.road_symbology_size_sql_selection.value,
         output_road_buffer_base=Building_N100.line_to_buffer_symbology___buffer_symbology___n100_building.value,
-        buffer_factors=[0.5, 0.75, 1],
         building_symbol_dimensions=N100_Symbology.building_symbol_dimensions.value,
     )
     point_displacement.run()
