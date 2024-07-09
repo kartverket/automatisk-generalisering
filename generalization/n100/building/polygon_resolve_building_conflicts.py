38a695cb37513c2956f8aa25f7026f0a27bc0725
EllingOftedalKV <elling.aronsen.oftedal@kartverket.no>
2024-07-09 20:25:30 +0200

    change to reflect file_manager_buildings.py refactor

--- generalization/n100/building/polygon_resolve_building_conflicts.py
+++ generalization/n100/building/polygon_resolve_building_conflicts.py
@@ -92,7 +92,7 @@ def roads_and_water_barriers_500_m_from_building_polygons():
     print("Selecting features 500 meter from building polygon ...")
     # Selecting begrensningskurve 500 meters from building polygons
     custom_arcpy.select_location_and_make_permanent_feature(
-        input_layer=Building_N100.data_preperation___waterfeatures_from_begrensningskurve_not_rivers___n100_building.value,
+        input_layer=Building_N100.data_preparation___waterfeatures_from_begrensningskurve_not_rivers___n100_building.value,
         overlap_type=custom_arcpy.OverlapType.WITHIN_A_DISTANCE,
         select_features=Building_N100.polygon_propogate_displacement___building_polygons_after_displacement___n100_building.value,
         output_name=Building_N100.polygon_resolve_building_conflicts___begrensningskurve_500m_from_displaced_polygon___n100_building.value,
