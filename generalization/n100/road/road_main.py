21a548ca4037d7a51296355c9dbf07df4384b420
maribhe <mari.helland@kartverket.no>
2024-08-07 08:36:19 +0200

    main

---
+++ generalization/n100/road/road_main.py
@@ -0,0 +1,16 @@
+# Importing modules
+from env_setup import environment_setup
+from custom_tools.decorators.timing_decorator import timing_decorator
+
+
+# Importing road scripts
+
+
+# Main function that runs all the road scripts
+@timing_decorator
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
