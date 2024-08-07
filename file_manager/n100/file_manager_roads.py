5c42eecfd84de87f908a2a8dfcbdac8834a400b9
maribhe <mari.helland@kartverket.no>
2024-08-07 08:35:45 +0200

    Example docstring

--- file_manager/n100/file_manager_roads.py
+++ file_manager/n100/file_manager_roads.py
@@ -15,8 +15,42 @@ data_selection = "data_selection"
 data_preparation = "data_preparation"
 
 
-class RoadN100(Enum):
+""""
+# gdb
     data_selection___example___n100_road = file_manager.generate_file_name_gdb(
         script_source_name=data_selection,
         description="example",
     )
+    
+# csv
+    data_selection___example_csv___n100_road = (
+        file_manager.generate_file_name_general_files(
+            script_source_name="data_selection",
+            description="example_csv",
+            file_type="csv",
+        )
+    )
+# lyrx
+    data_selection___example_lyrx___n100_road_lyrx = (
+        file_manager.generate_file_name_lyrx(
+            script_source_name="data_selection",
+            description="example_lyrx___n100_road",
+        )
+    )
+# final output
+    Road = file_manager.generate_final_outputs(
+        file_name="Road",
+    )
+    
+# lyrx directory
+    data_selection___lyrx_root___n100_road= (
+        file_manager.generate_file_lyrx_directory(
+            script_source_name=data_selection, description="lyrx_root"
+        )
+    )
+
+"""
+
+
+class RoadN100(Enum):
+    pass
