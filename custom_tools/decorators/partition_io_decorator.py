a6adb713cffa95fe1fd59559b1ce6939c4a5e2ee
EllingOftedalKV <elling.aronsen.oftedal@kartverket.no>
2024-09-17 13:51:04 +0200

    Updates to docstring

--- custom_tools/decorators/partition_io_decorator.py
+++ custom_tools/decorators/partition_io_decorator.py
@@ -1,4 +1,22 @@
-def partition_io_decorator(input_param_names=None, output_param_names=None):
+from typing import Callable, List, Optional
+
+
+def partition_io_decorator(
+    input_param_names: Optional[List[str]] = None,
+    output_param_names: Optional[List[str]] = None,
+) -> Callable:
+    """
+    What:
+        A decorator that adds metadata about input and output parameters for partitioning logic functions.
+
+    Args:
+        input_param_names (Optional[List[str]]): A list of input parameter names for the decorated function.
+        output_param_names (Optional[List[str]]): A list of output parameter names for the decorated function.
+
+    Returns:
+        Callable: The decorated function with added metadata attributes for inputs and outputs.
+    """
+
     def decorator(func):
         setattr(
             func,
