013c0abefaa6e6b98b19d79a9cc711d35cc3d39e
maribhe <mari.helland@kartverket.no>
2024-09-25 13:54:48 +0200

    .

--- generalization/n100/road/road_main.py
+++ generalization/n100/road/road_main.py
@@ -5,12 +5,14 @@ from custom_tools.decorators.timing_decorator import timing_decorator
 
 # Importing road scripts
 from generalization.n100.road import data_preparation
+from generalization.n100.road import first_generalization
 
 
 # Main function that runs all the road scripts
 @timing_decorator
 def main():
     data_preparation.main()
+    first_generalization.main()
 
 
 if __name__ == "__main__":
