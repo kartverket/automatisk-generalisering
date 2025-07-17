3cf70a075fafb93732e377bdb5107dc70be4bd2e
EllingOftedalKV <elling.aronsen.oftedal@kartverket.no>
2025-07-17 15:30:48 +0200

    Refactoring work_file_manager_config into project

--- composition_configs/base_logic_config.py
+++ composition_configs/base_logic_config.py
@@ -4,6 +4,16 @@ from typing import Dict, List, Optional, Tuple, Union
 
 @dataclass
 class WorkFileConfig:
+    """
+    Configuration object for work file management behavior.
+
+    Args:
+        root_file (str): The core file name used to generate unique file names.
+        write_to_memory (bool): If True, files are written to memory, if False, written to disk.
+        keep_files (bool): If True, work files are kept after execution; if False, they are deleted when done.
+
+    """
+
     root_file: str
-    write_to_memory: Optional[bool] = False
-    keep_files: Optional[bool] = False
+    write_to_memory: bool = False
+    keep_files: bool = False
