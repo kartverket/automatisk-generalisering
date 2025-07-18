f559688d73b0f5cc7a93ba6f1b97d44123a91fd8
EllingOftedalKV <elling.aronsen.oftedal@kartverket.no>
2025-07-18 19:59:37 +0200

    initial partition io config

--- composition_configs/core_config.py
+++ composition_configs/core_config.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import Dict, List, Optional, Tuple, Union
+from enum import Enum
 
 
 @dataclass
@@ -17,3 +18,116 @@ class WorkFileConfig:
     root_file: str
     write_to_memory: bool = False
     keep_files: bool = False
+
+
+class IOType(Enum):
+    INPUT = "input"
+    CONTEXT = "context"
+    REFERENCE = "reference"
+
+
+@dataclass(frozen=True)
+class InputEntry:
+    type: IOType
+    path: str
+
+
+@dataclass(frozen=True)
+class OutputEntry:
+    tag: str
+    path: str
+
+
+@dataclass
+class PartitionInputConfig:
+    inputs: Dict[str, InputEntry]
+
+
+@dataclass
+class PartitionOutputConfig:
+    outputs: Dict[str, OutputEntry]
+
+
+@dataclass
+class PartitionIOConfig:
+    input_config: PartitionInputConfig
+    output_config: PartitionOutputConfig
+    dictionary_documentation_path: Optional[str] = None
+
+
+test_input = PartitionInputConfig(
+    inputs={
+        "road": InputEntry(
+            type=IOType.INPUT,
+            path="test_input_path",
+        ),
+        "building": InputEntry(
+            type=IOType.CONTEXT,
+            path="test_input_path",
+        ),
+    }
+)
+
+test_output = PartitionOutputConfig(
+    outputs={
+        "road": OutputEntry(
+            tag="merge_output",
+            path="output_path",
+        )
+    }
+)
+
+partition_io_config = PartitionIOConfig(
+    input_config=test_input,
+    output_config=test_output,
+)
+
+partition_io_2 = PartitionIOConfig(
+    input_config=PartitionInputConfig(
+        inputs={
+            "railroad": InputEntry(
+                type=IOType.INPUT,
+                path="some_path",
+            )
+        }
+    ),
+    output_config=PartitionOutputConfig(
+        {
+            "railroad": OutputEntry(
+                tag="some_output",
+                path="some_path",
+            )
+        }
+    ),
+)
+
+input_2 = PartitionInputConfig(
+    inputs={
+        "some_object": InputEntry(
+            type=IOType.INPUT,
+            path="some_path",
+        )
+    }
+)
+
+output_2 = PartitionOutputConfig(
+    outputs={
+        "some_object": OutputEntry(
+            tag="some_output",
+            path="some_path",
+        )
+    }
+)
+
+test_io_config_2 = PartitionIOConfig(
+    input_config=input_2,
+    output_config=output_2,
+)
+
+
+@dataclass
+class TempPartConfig:
+    alias_path_data: Dict[str, Tuple[Literal["input", "context", "reference"], str]]
+    alias_path_outputs: Dict[str, Tuple[str, str]]
+    custom_functions: Optional[List] = None
+    dictionary_documentation_path: Optional[str] = None
