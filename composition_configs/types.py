93084759934b706e9a301300739e52cff418be6d
EllingOftedalKV <elling.aronsen.oftedal@kartverket.no>
2025-08-19 17:58:11 +0200

    Removed types to not conflict with python std lib

--- composition_configs/types.py
+++
@@ -1,5 +0,0 @@
-from typing import NewType
-
-SubdirectoryPath = NewType(name="SubdirectoryPath", tp=str)
-GdbFilePath = NewType(name="GdbFilePath", tp=str)
-LyrxFilePath = NewType(name="LyrxFilePath", tp=str)
