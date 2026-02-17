c6b6b4cd18db8b8e3e861586ae27f8e6eddcafbc
EllingOftedalKV <elling.aronsen.oftedal@kartverket.no>
2026-02-17 13:54:34 +0100

    topology module for connectivity scope

--- generalization/n100/river/fix_river_topology_gaps.py
+++ generalization/n100/river/fix_river_topology_gaps.py
@@ -3,6 +3,7 @@ import arcpy
 # Importing custom files
 import config
 from custom_tools.general_tools import custom_arcpy, line_topology
+from file_manager import work_file_manager
 from file_manager.n100.file_manager_rivers import River_N100
 
 from env_setup import environment_setup
@@ -30,16 +31,21 @@ def fill_line_topology_gaps():
         fill_gaps_on_self=True,
         line_changes_output=River_N100.river_topology___river_gaps_changes___n100_river.value,
         increased_tolerance_edge_case_distance_meters=3,
+        edit_method=logic_config.EditMethod.AUTO,
+        connectivity_scope=logic_config.ConnectivityScope.TRANSITIVE,
+        connectivity_tolerance_meters=environment_setup.ArcGisEnvironmentSetup.XY_TOLERANCE,
+        line_connectivity_mode=logic_config.LineConnectivityMode.ENDPOINTS,
+    )
+    work_file_manager_config = core_config.WorkFileConfig(
+        root_file=River_N100.river_topology___root___n100_river.value,
+        write_to_memory=False,
+        keep_files=True,
     )
 
     line_fix_config = logic_config.FillLineGapsConfig(
         input_lines=River_N100.data_preparation___river_lines___n100_river.value,
         output_lines=River_N100.river_topology___fixed_river_gaps___n100_river.value,
-        work_file_manager_config=core_config.WorkFileConfig(
-            root_file=River_N100.river_topology___root___n100_river.value,
-            write_to_memory=False,
-            keep_files=True,
-        ),
+        work_file_manager_config=work_file_manager_config,
         gap_tolerance_meters=3,
         connect_to_features=[
             River_N100.data_preparation___river_polygons___n100_river.value
