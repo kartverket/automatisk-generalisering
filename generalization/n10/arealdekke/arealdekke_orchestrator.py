fa5036dcaf8ec7dedc79683daaaebe973e7d6713
jhjelz <jakob.hjelseth@kartverket.no>
2026-03-26 13:11:23 +0100

    Setup created

--- generalization/n10/arealdekke/arealdekke_orchestrator.py
+++ generalization/n10/arealdekke/arealdekke_orchestrator.py
@@ -9,8 +9,9 @@ from generalization.n10.arealdekke.eliminate_small_polygons import (
 )
 from generalization.n10.arealdekke.attribute_changer import attribute_changer
 from generalization.n10.arealdekke.island_controller import island_controller
+from generalization.n10.arealdekke.simplify_lakes import simplify_lakes
 
-from input_data import input_n10
+from input_data import input_n10, input_test_data
 from file_manager.n10.file_manager_arealdekke import Arealdekke_N10
 from env_setup import environment_setup
 from custom_tools.decorators.timing_decorator import timing_decorator
@@ -21,9 +22,17 @@ def main():
     MAP_SCALE = "N10"
     environment_setup.main()
     attribute_changer(
-        input_fc=input_n10.Arealdekke_Oslo,
+        input_fc=input_test_data.arealdekke,  # input_n10.Arealdekke_Oslo,
         output_fc=Arealdekke_N10.attribute_changer_output__n10_land_use.value,
     )
+
+    simplify_lakes(
+        input_fc=Arealdekke_N10.attribute_changer_output__n10_land_use.value,
+        output_fc=Arealdekke_N10.simplify_lakes_output__n10_land_use.value,
+    )
+
+    return
+
     arealdekke_dissolver(
         input_fc=Arealdekke_N10.attribute_changer_output__n10_land_use.value,
         output_fc=Arealdekke_N10.dissolve_arealdekke.value,
