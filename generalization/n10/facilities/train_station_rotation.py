00a9762d1b1083a938b449d4bbfa1773629e4ce2
jhjelz <jakob.hjelseth@kartverket.no>
2026-04-30 09:23:40 +0200

    Fix black

--- generalization/n10/facilities/train_station_rotation.py
+++ generalization/n10/facilities/train_station_rotation.py
@@ -28,72 +28,112 @@ def main():
 @timing_decorator
 def create_wfm_gdbs(wfm: WorkFileManager) -> dict:
 
-    train_track = wfm.build_file_path(
-        file_name="train_tracks", file_type="gdb"
+    train_track = wfm.build_file_path(file_name="train_tracks", file_type="gdb")
+    train_station = wfm.build_file_path(file_name="train_station", file_type="gdb")
+    train_station_snapped = wfm.build_file_path(
+        file_name="train_station_snapped", file_type="gdb"
     )
-    train_station = wfm.build_file_path(
-        file_name="train_station", file_type="gdb"
+    train_station_buffer = wfm.build_file_path(
+        file_name="train_station_buffer", file_type="gdb"
     )
-    train_station_snapped = wfm.build_file_path(file_name="train_station_snapped", file_type="gdb")
-    train_station_buffer = wfm.build_file_path(file_name="train_station_buffer", file_type="gdb")
-    track_segment_multi = wfm.build_file_path(file_name="track_segment_multi", file_type="gdb")
-    track_segment_single = wfm.build_file_path(file_name="track_segment_single", file_type="gdb")
-    train_station_rotated = wfm.build_file_path(file_name="train_station_rotated", file_type="gdb")
+    track_segment_multi = wfm.build_file_path(
+        file_name="track_segment_multi", file_type="gdb"
+    )
+    track_segment_single = wfm.build_file_path(
+        file_name="track_segment_single", file_type="gdb"
+    )
+    train_station_rotated = wfm.build_file_path(
+        file_name="train_station_rotated", file_type="gdb"
+    )
 
     return {
         "train_track": train_track,
         "train_station": train_station,
-        "train_station_snapped":train_station_snapped,
+        "train_station_snapped": train_station_snapped,
         "train_station_buffer": train_station_buffer,
-        "track_segment_multi":track_segment_multi,
-        "track_segment_single":track_segment_single,
-        "train_station_rotated":train_station_rotated
+        "track_segment_multi": track_segment_multi,
+        "track_segment_single": track_segment_single,
+        "train_station_rotated": train_station_rotated,
     }
 
 
 @timing_decorator
 def fetch_data(files: dict) -> None:
     arcpy.management.CopyFeatures(
-        in_features=input_n10.bane,
-        out_feature_class=files["train_track"]
+        in_features=input_n10.bane, out_feature_class=files["train_track"]
+    )
+    arcpy.management.CopyFeatures(
+        in_features=input_n10.jernbanestasjon, out_feature_class=files["train_station"]
     )
     arcpy.management.CopyFeatures(
         in_features=input_n10.jernbanestasjon,
-        out_feature_class=files["train_station"]
+        out_feature_class=files["train_station_snapped"],
     )
-    arcpy.management.CopyFeatures(
-        in_features=input_n10.jernbanestasjon,
-        out_feature_class=files["train_station_snapped"]
-    )
+
 
 @timing_decorator
-def find_track_rotation(files:dict)->None:
-    arcpy.edit.Snap(files["train_station_snapped"], [[files["train_track"], 'EDGE', '40 Meters']])
-    arcpy.analysis.Buffer(files["train_station_snapped"], files["train_station_buffer"], '5 Meters', 'FULL')
-    
-    tracks_under_station=arcpy.management.SelectLayerByLocation(files["train_track"], 'INTERSECT', files["train_station_snapped"], None, 'NEW_SELECTION')
-    tracks_under_station_layer=arcpy.management.MakeFeatureLayer(tracks_under_station, "tracks_under_station_layer")
+def find_track_rotation(files: dict) -> None:
+    arcpy.edit.Snap(
+        files["train_station_snapped"], [[files["train_track"], "EDGE", "40 Meters"]]
+    )
+    arcpy.analysis.Buffer(
+        files["train_station_snapped"],
+        files["train_station_buffer"],
+        "5 Meters",
+        "FULL",
+    )
 
-    arcpy.analysis.Clip(tracks_under_station_layer, files["train_station_buffer"], files["track_segment_multi"])
-    arcpy.management.MultipartToSinglepart(files["track_segment_multi"], files["track_segment_single"])
-    arcpy.management.CalculateGeometryAttributes(files["track_segment_single"], [['LINE_BEARING', 'LINE_BEARING']])
+    tracks_under_station = arcpy.management.SelectLayerByLocation(
+        files["train_track"],
+        "INTERSECT",
+        files["train_station_snapped"],
+        None,
+        "NEW_SELECTION",
+    )
+    tracks_under_station_layer = arcpy.management.MakeFeatureLayer(
+        tracks_under_station, "tracks_under_station_layer"
+    )
 
-    arcpy.analysis.SpatialJoin(files["train_station_snapped"], files["track_segment_single"], files["train_station_rotated"], 'JOIN_ONE_TO_ONE', 'KEEP_ALL', None, 'INTERSECT')
+    arcpy.analysis.Clip(
+        tracks_under_station_layer,
+        files["train_station_buffer"],
+        files["track_segment_multi"],
+    )
+    arcpy.management.MultipartToSinglepart(
+        files["track_segment_multi"], files["track_segment_single"]
+    )
+    arcpy.management.CalculateGeometryAttributes(
+        files["track_segment_single"], [["LINE_BEARING", "LINE_BEARING"]]
+    )
 
-    new_field_name="ROTASJON"
+    arcpy.analysis.SpatialJoin(
+        files["train_station_snapped"],
+        files["track_segment_single"],
+        files["train_station_rotated"],
+        "JOIN_ONE_TO_ONE",
+        "KEEP_ALL",
+        None,
+        "INTERSECT",
+    )
+
+    new_field_name = "ROTASJON"
     arcpy.management.AddField(files["train_station"], new_field_name)
 
-    with arcpy.da.UpdateCursor(files["train_station"], ["OBJECTID", new_field_name]) as original_cursor:
+    with arcpy.da.UpdateCursor(
+        files["train_station"], ["OBJECTID", new_field_name]
+    ) as original_cursor:
         for original_station in original_cursor:
 
-            with arcpy.da.SearchCursor(files["train_station_rotated"], ["OBJECTID", "LINE_BEARING"]) as copy_cursor:
+            with arcpy.da.SearchCursor(
+                files["train_station_rotated"], ["OBJECTID", "LINE_BEARING"]
+            ) as copy_cursor:
                 for copy_station in copy_cursor:
 
-                    if original_station[0]==copy_station[0]:
-                        original_station[1]=copy_station[1]
+                    if original_station[0] == copy_station[0]:
+                        original_station[1] = copy_station[1]
                         original_cursor.updateRow(original_station)
                         break
-            
+
 
 if __name__ == "__main__":
     main()
