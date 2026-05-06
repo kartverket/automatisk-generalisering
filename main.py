044b36750e439b9f652f49cf16cebe9c60129534
KV-EvenFH <even.fuglesang@kartverket.no>
2026-05-06 13:40:59 +0200

    devcontainer, cmd entry, read-only check

--- main.py
+++ main.py
@@ -12,11 +12,42 @@ logging.basicConfig(
 
 logger = logging.getLogger(__name__)
 
+def check_uid_gid():
+    current_uid = os.getuid()
+    current_gid = os.getgid()
+    logger.info("current uid: %s current gid: %s", current_uid, current_gid)
+
+def check_read_only():
+    tmp_file = "/tmp/file.txt"
+    with open(tmp_file, "w", encoding="utf-8") as f:
+        f.write("written to and read from /tmp")
+    
+    with open(tmp_file, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    logger.info(content)
+    os.remove(tmp_file)
+
+    root_file = "file.txt"
+    try:
+        with open(root_file, "w", encoding="utf-8") as f:
+            f.write("written to and read from root")
+            
+        with open(root_file, "r", encoding="utf-8") as f:
+            content2 = f.read()
+        logger.info(content2)
+        os.remove(root_file)
+    except Exception as e: 
+        logger.info("Failed to write to root: ")
+        logger.info(e)
+
+    
+
 #pipeline imports
 def pipeline_n100_road(args: argparse.Namespace) -> None:
-    #from generalization.n100.road.data_preparation_2 import run as run_n100_road
+    from generalization.n100.road.data_preparation_2 import run as run_n100_road
     logger.info("Starting pipeline for %s", args)
-    #run_n100_road()
+    run_n100_road()
 
 
 def pipeline_n10_arealdekke(args: argparse.Namespace) -> None:
@@ -44,6 +75,8 @@ def parse_args():
 
 def main():
     args = parse_args()
+    check_uid_gid()
+    check_read_only()
 
     if not args.scale or not args.obj:
         logger.error("Error: --scale and --object required (or set SCALE/OBJECT env)")
