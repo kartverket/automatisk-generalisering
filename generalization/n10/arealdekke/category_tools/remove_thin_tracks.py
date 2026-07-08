7476c9c5361bfbc2ca411245451bae8e9ef2b3fc
jhjelz <jakob.hjelseth@kartverket.no>
2026-07-08 15:28:38 +0200

    Start

---
+++ generalization/n10/arealdekke/category_tools/remove_thin_tracks.py
@@ -0,0 +1,25 @@
+# Libraries
+
+import arcpy
+
+# ========================
+# Main function
+# ========================
+
+
+def remove_thin_tracks(input_fc, output_fc, min_width):
+    return
+
+
+# ========================
+# Helper functions
+# ========================
+
+
+# ...
+
+
+# ========================
+
+if __name__ == "__main__":
+    pass
