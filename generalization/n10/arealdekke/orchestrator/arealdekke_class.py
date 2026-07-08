fbb3a742e196ce0dc4b688a6d2e8ce0b3bef070e
jhjelz <jakob.hjelseth@kartverket.no>
2026-07-08 15:02:45 +0200

    Proposal for complete setup

--- generalization/n10/arealdekke/orchestrator/arealdekke_class.py
+++ generalization/n10/arealdekke/orchestrator/arealdekke_class.py
@@ -427,6 +427,7 @@ class Arealdekke:
             keep = {
                 Path(self.final_output_fc).name,
                 Path(Arealdekke_N10.passability__n10_land_use.value).name,
+                Path(Arealdekke_N10.poly_to_point_points__n10_land_use.value).name,
             }
 
             arcpy.env.workspace = str(gdb)
