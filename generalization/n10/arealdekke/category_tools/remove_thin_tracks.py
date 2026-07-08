d5cff6f5722823b6d6b074a54826da1f13c77b16
jhjelz <jakob.hjelseth@kartverket.no>
2026-07-08 15:45:55 +0200

    Sketched test setup

--- generalization/n10/arealdekke/category_tools/remove_thin_tracks.py
+++ generalization/n10/arealdekke/category_tools/remove_thin_tracks.py
@@ -2,12 +2,29 @@
 
 import arcpy
 
+from file_manager.n10.file_manager_arealdekke import Arealdekke_N10
+
 # ========================
 # Main function
 # ========================
 
 
-def remove_thin_tracks(input_fc, output_fc, min_width):
+def remove_thin_tracks(
+    target: str,
+    input_fc: str,
+    output_fc: str,
+    locked_fc: str,
+    complete_fc: str,
+    map_scale: str,
+):
+    """
+    For areas of type target, remove all thin tracks of type "Samferdsel"
+    that goes through the area. This area should be replaced with the target
+    area.
+
+    Args:
+        ...
+    """
     return
 
 
@@ -22,4 +39,13 @@ def remove_thin_tracks(input_fc, output_fc, min_width):
 # ========================
 
 if __name__ == "__main__":
-    pass
+    relevant_features = ["Bergverk", "GravUrnelund", "Industri", "Jordbruk"]
+
+    target = relevant_features[0]
+    input_fc = Arealdekke_N10.dissolve_gangsykkel.value
+
+    lyr = f"{target}_lyr"
+    arcpy.MakeFeatureLayer_management(input_fc, lyr)
+    arcpy.SelectLayerByAttribute_management(
+        lyr, "NEW_SELECTION", f"arealdekke = '{target}'"
+    )
