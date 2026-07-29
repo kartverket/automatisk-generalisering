307efbb247ca1a59e81b324c50282bf160133a12
jhjelz <jakob.hjelseth@kartverket.no>
2026-07-29 14:42:35 +0200

    Fix

--- file_manager/n10/file_manager_arealdekke.py
+++ file_manager/n10/file_manager_arealdekke.py
@@ -32,7 +32,6 @@ small_features_changer = "small_features_changer"
 poly_to_point = "poly_to_point"
 remove_thin_tracks = "remove_thin_tracks"
 river_lines = "river_lines"
-smooth_transition = "smooth_transition"
 snap_lines = "snap_lines"
 
 
@@ -172,14 +171,6 @@ class Arealdekke_N10(Enum):
 
     river_lines__n10_land_use = file_manager.generate_file_name_gdb(
         script_source_name=river_lines, description="river_lines"
-    )
-
-    # ========================================
-    #              BUFF SMALL POLYGON SEGMENTS
-    # ========================================
-
-    smooth_transition__n10_land_use = file_manager.generate_file_name_gdb(
-        script_source_name=smooth_transition, description="smooth_transition"
     )
 
     # ========================================
